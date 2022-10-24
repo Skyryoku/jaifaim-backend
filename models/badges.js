ff7b8d4e05c5b3631438d98e182512f9af8ad7a3
Chloe <chloeheaulme@gmail.com>
2022-10-24 10:29:21 +0200

    models

---
+++ models/badges.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const badgeSchema = mongoose.Schema({
+
+    name: String,
+    restaurants: { type: mongoose.Schema.Types.ObjectId, ref: 'restaurants'},
+    source: String
+
+   
+    
+  });
+  
+  const Badge = mongoose.model('badges', badgeSchema);
+  
+  module.exports = Badge;
---
+++ models/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  email: String,
+  firstname: String,
+  password: String,
+  token: String,
+  diets: String,
+  intolerances: String,
+  profilGourmand: String,
+  badges:{ type: mongoose.Schema.Types.ObjectId, ref: 'badges'},
+  
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
