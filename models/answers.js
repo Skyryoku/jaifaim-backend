ede3ac94366b20c305c8b8caf17d12f7a377fbd3
Lydie Paulo <lydiepaulo.ui@gmail.com>
2022-10-24 11:07:43 +0200

    model answers

---
+++ models/answers.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const answerSchema = mongoose.Schema({
+
+    question: [{ type: mongoose.Schema.Types.ObjectId, ref: 'questions' }],
+    date: Date,
+    message: String,
+
+});
+
+const Answer = mongoose.model('answers', answerSchema);
+
+module.exports = Answer;
